1ae9e0e4d1b7f9da008bf10665af0d29bae0bc8c
Rastislav Wagner <rawagner@redhat.com>
2016-02-10 13:16:29 +0100

    Database connection working

--- src/main/java/org/jboss/tools/f2f/data/WordRepository.java
+++ src/main/java/org/jboss/tools/f2f/data/WordRepository.java
@@ -1,22 +1,42 @@
 package org.jboss.tools.f2f.data;
 
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
+import org.bson.Document;
 import org.jboss.tools.f2f.model.Word;
+
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.util.JSON;
 
 @ApplicationScoped
 public class WordRepository {
 	
-	@Inject
-    private EntityManager em;
-	
-	public List<Word> getWords(){
-		List<Word> words = em.createQuery("SELECT w FROM Words w").getResultList();
-		return words;
+	public List<String> getWords(){
+		 MongoClientURI uri = new MongoClientURI("mongodb://redteam:redpassword@ds061415.mongolab.com:61415/buzzword");
+		 MongoClient mg = null;
+		 mg =  new MongoClient(uri);
+		 
+		 
+		 DBCollection coll = mg.getDB("buzzword").getCollection("raw");
+		 DBCursor cur = coll.find();
+		 List<String> jsonWords = new ArrayList<String>();
+		 while (cur.hasNext()) {  
+			 String jsonWord = JSON.serialize(cur);
+			 jsonWords.add(jsonWord);
+	      }
+		 mg.close();
+		 return jsonWords;
 	}
 
 }
